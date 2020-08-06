653620e055b5b4aedaff6bbb00f7ec7f15d2b2f7
龙方淞 <longfangsong@icloud.com>
2020-08-06 15:36:01 +0800

    Add integration test for basic crud (#56)
    * Add integration test for basic crud
    
    Signed-off-by: longfangsong <longfangsong@icloud.com>

---
+++ integration/smoke/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+bash ./integration/utils/start-server.sh ./integration/smoke/api.yml
+# shellcheck disable=SC2046
+if [ $(curl http://localhost:8000/ping) == "pong" ]; then
+  exit 0;
+else
+  exit 1;
+fi
+
