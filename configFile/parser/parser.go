070476a7796a6062d32685416ef1259b6bd5be08
Andrewmatilde <davis6813585853062@outlook.com>
2020-08-08 12:30:30 +0800

    use .sql file now and add reg test

--- configFile/parser/parser.go
+++ configFile/parser/parser.go
@@ -9,31 +9,58 @@ import (
 	"github.com/pingcap/parser/ast"
 	"github.com/pingcap/parser/types"
 	_ "github.com/pingcap/tidb/types/parser_driver"
+	"io/ioutil"
 	"regexp"
 	"strings"
+	"unsafe"
 )
 
-func FillTableInfo(s string, tb *database.Table) error {
-	parser := parser.New()
-	s = fixSerial(s)
-	stmt, err := parser.ParseOneStmt(s, "", "")
+func FillTableInfo(path string, tb *database.Table) error {
+	s, err := getSqlString(path)
 	if err != nil {
 		return err
 	}
-	ctStmt, ok := stmt.(*ast.CreateTableStmt)
-	if !ok {
-		return errors.New("change type to CreateTableStmt fail")
+	parser := parser.New()
+	stmts, _, err := parser.Parse(s, "", "")
+	if err != nil {
+		return err
 	}
-	for _, col := range ctStmt.Cols {
-		tb.Fields = append(tb.Fields, field.Field{Name: withCase.New(col.Name.Name.L),
-			Type: types.TypeStr(col.Tp.Tp)})
+	for _, stmt := range stmts {
+		ctStmt, ok := stmt.(*ast.CreateTableStmt)
+		if !ok {
+			return errors.New("change type to CreateTableStmt fail")
+		}
+		for _, col := range ctStmt.Cols {
+			tb.Fields = append(tb.Fields, field.Field{Name: withCase.New(col.Name.Name.L),
+				Type: types.TypeStr(col.Tp.Tp)})
+		}
+		w := withCase.New(ctStmt.Table.Name.L)
+		tb.Name = &w
 	}
-	w := withCase.New(ctStmt.Table.Name.L)
-	tb.Name = &w
 	return nil
 }
 
+func getSqlString(path string) (string, error) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	s := toString(b)
+	s = fixSerial(s)
+	s = fixBigSerial(s)
+	return s, nil
+}
+
+func toString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
 func fixSerial(s string) string {
-	reg1 := regexp.MustCompile("([\b,\t\n]+)(serial)([\b,\t\n]+)")
+	reg1 := regexp.MustCompile("([,\\s\t\n]+)(serial)([,\\s\t\n]+)")
 	return reg1.ReplaceAllString(strings.ToLower(s), "${1}int${3}")
+}
+
+func fixBigSerial(s string) string {
+	reg1 := regexp.MustCompile("([,\\s\t\n]+)(bigserial)([,\\s\t\n]+)")
+	return reg1.ReplaceAllString(strings.ToLower(s), "${1}bigint${3}")
 }
