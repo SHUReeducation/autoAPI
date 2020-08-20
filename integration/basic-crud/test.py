f02c6e4188918016e29ff5bb1582e59f3800421b
龙方淞 <longfangsong@icloud.com>
2020-08-21 06:54:32 +0800

    refactor out tests (#73)
    * refactor out tests
    
    * Update integration-tests.yml

--- integration/basic-crud/test.py
+++ integration/basic-crud/test.py
@@ -1,14 +1,18 @@
+import json
+import subprocess
+import sys
 import unittest
 import urllib
-import urllib.request
 import urllib.error
-import subprocess
-import json
+import urllib.request
 
-class BasicCRUDTest(unittest.TestCase):
+
+class CRUDTest(unittest.TestCase):
+    api_path = ""
+
     @classmethod
     def setUpClass(cls):
-        subprocess.run('bash ./integration/utils/start-server.sh ./integration/basic-crud/api.yml'.split(' '))
+        subprocess.run('bash ./integration/utils/start-server.sh {}'.format(cls.api_path).split(' '))
 
     def test_get(self):
         response = urllib.request.urlopen('http://localhost:8000/students/1').read().decode('utf-8')
@@ -27,16 +31,18 @@ class BasicCRUDTest(unittest.TestCase):
         old_count = len(json.loads(response))
 
         data = json.dumps({'name': 'E', 'birthday': '1990-05-05T00:00:00Z', 'school_id': 5})
-        request = urllib.request.Request(url='http://localhost:8000/students', data=data.encode(encoding='utf-8'), method='POST')
+        request = urllib.request.Request(url='http://localhost:8000/students', data=data.encode(encoding='utf-8'),
+                                         method='POST')
         response = urllib.request.urlopen(request).read().decode('utf-8')
         student_post_result = json.loads(response)
-        response = urllib.request.urlopen('http://localhost:8000/students/{}'.format(student_post_result['id'])).read().decode('utf-8')
+        response = urllib.request.urlopen(
+            'http://localhost:8000/students/{}'.format(student_post_result['id'])).read().decode('utf-8')
         student_get_result = json.loads(response)
         self.assertDictEqual(student_post_result, student_get_result)
 
         response = urllib.request.urlopen('http://localhost:8000/students?limit=100&offset=0').read().decode('utf-8')
         new_count = len(json.loads(response))
-        self.assertEqual(new_count, old_count+1)
+        self.assertEqual(new_count, old_count + 1)
 
     def test_put(self):
         response = urllib.request.urlopen('http://localhost:8000/students?limit=100&offset=0').read().decode('utf-8')
@@ -44,7 +50,8 @@ class BasicCRUDTest(unittest.TestCase):
 
         new_info = {'id': 1, 'name': 'F', 'birthday': '1990-06-06T00:00:00Z', 'school_id': 6}
         data = json.dumps(new_info)
-        request = urllib.request.Request(url='http://localhost:8000/students/1', data=data.encode(encoding='utf-8'), method='PUT')
+        request = urllib.request.Request(url='http://localhost:8000/students/1', data=data.encode(encoding='utf-8'),
+                                         method='PUT')
         urllib.request.urlopen(request)
         response = urllib.request.urlopen('http://localhost:8000/students/1').read().decode('utf-8')
         student = json.loads(response)
@@ -66,10 +73,11 @@ class BasicCRUDTest(unittest.TestCase):
 
         response = urllib.request.urlopen('http://localhost:8000/students?limit=100&offset=0').read().decode('utf-8')
         new_count = len(json.loads(response))
-        self.assertEqual(new_count, old_count-1)
+        self.assertEqual(new_count, old_count - 1)
 
 
 if __name__ == '__main__':
+    CRUDTest.api_path = sys.argv[1]
     loader = unittest.TestLoader()
     loader.sortTestMethodsUsing = None
-    unittest.main(testLoader=loader)
+    unittest.main(argv=[sys.argv[0]], testLoader=loader)
